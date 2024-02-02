224b0df469a23c56e12ba1b78921f625ac9f641a
Natnael Taddese Teferra <101579499+NatnaelTaddese@users.noreply.github.com>
2024-02-03 03:25:32 +0800

    Time: 12 ms (87.65%), Space: 11.7 MB (88.78%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -4,13 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        n_good_pairs = 0
-        good_pairs = set()
+        good_pairs = 0
+        seen_pairs = set() # keeps track of the pairs, to remove redundant pairs
 
         for i in range(len(nums) - 1):
            for j in range(i + 1, len(nums)):
-               if nums[i] == nums[j] and (i, j) not in good_pairs:
-                   n_good_pairs += 1
-                   good_pairs.add((i, j))
+               if nums[i] == nums[j] and (i, j) not in seen_pairs:
+                   good_pairs += 1
+                   seen_pairs.add((i, j))
 
-        return n_good_pairs
+        return good_pairs
