496a358035fc4c0b3f7056ab008b55ffb5243c3c
Natnael Taddese Teferra <101579499+NatnaelTaddese@users.noreply.github.com>
2024-02-17 01:43:12 +0800

    Time: 1404 ms (69.38%), Space: 69.9 MB (99.66%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -4,6 +4,37 @@ class Solution(object):
         :type matches: List[List[int]]
         :rtype: List[List[int]]
         """
+        # Brute force method:
+
+        # no_losses = set()
+        # only_one_lose = set()
+
+        # for i in range(len(matches) ):
+
+        #     loss = 0
+        #     only_one = 0
+
+        #     for j in range(0, len(matches)):
+        #         if matches[i][0] == matches[j][1]:
+        #             loss += 1
+        #         if matches[i][1] == matches[j][1]:
+        #             only_one += 1
+
+        #     if loss == 0:
+        #         no_losses.add(matches[i][0])
+            
+        #     if only_one == 1:
+        #         only_one_lose.add(matches[i][1])
+        
+        # no_losses = sorted(no_losses)
+        # only_one_lose = sorted(only_one_lose)
+
+        # # no_losses = set(no_losses)
+        # # only_one_lose = set(only_one_lose)
+        
+        # return[no_losses,only_one_lose]
+
+
         wins = dict()
         losses = dict()
 
@@ -31,5 +62,4 @@ class Solution(object):
         no_loss.sort()
         only_one_loss.sort()
         return [no_loss, only_one_loss]
-
         
