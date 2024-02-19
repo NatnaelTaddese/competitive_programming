40509cf8f0ff7e99f0dae5c500fe50cf3eeaaf0d
NatnaelTaddese <natnaeltaddese3@gmail.com>
2024-02-20 01:56:42 +0800

    didnt even try this one

--- C_Pairs.py
+++ C_Pairs.py
@@ -5,7 +5,3 @@ n = int(input())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 c = list(map(int, input().split()))
-
-# Given three arrays of length N each, a, b and c, consisting of integers between 1 and N
-# (inclusive), how many pairs (i,j) of integers between 1 and N(inclusive) satisfy a[i]=b[c[j]]
-
