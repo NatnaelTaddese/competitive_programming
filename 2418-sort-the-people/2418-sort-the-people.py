dab5f99dcc5dd7464ce7b35115b2004b64c99703
Natnael Taddese Teferra <101579499+NatnaelTaddese@users.noreply.github.com>
2024-02-28 03:17:08 +0800

    Time: 518 ms (14.84%), Space: 12 MB (97.72%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -5,18 +5,37 @@ class Solution(object):
         :type heights: List[int]
         :rtype: List[str]
         """
+        ######################
         # using bubble sort
-        for i in range(len(heights)):
-            swapped = False
 
-            for j in range(0, len(heights) - i - 1):
-                if heights[j] < heights[j + 1]:
-                    names[j], names[j+1] = names[j+1], names[j]
-                    heights[j], heights[j + 1] = heights[j + 1], heights[j]
-                    swapped = True
+        # for i in range(len(heights)):
+        #     swapped = False
+
+        #     for j in range(0, len(heights) - i - 1):
+        #         if heights[j] < heights[j + 1]:
+        #             names[j], names[j+1] = names[j+1], names[j]
+        #             heights[j], heights[j + 1] = heights[j + 1], heights[j]
+        #             swapped = True
             
-            if not swapped:
-                break
+        #     if not swapped:
+        #         break
+
+        ######################
+        # using insertion sort
+
+        for i in range(1, len(heights)):
+            key_height = heights[i]
+            key_name = names[i]
+
+            j = i - 1
+            while j >= 0 and heights[j] < key_height:
+                heights[j + 1] = heights[j]
+                names[j + 1] = names[j]
+                j -= 1
+            
+            heights[j + 1] = key_height
+            names[j + 1] = key_name
         
         return names
         
+
