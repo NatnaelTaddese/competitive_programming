adb771155800216defc6fece8002f86ac9240556
Natnael Taddese Teferra <101579499+NatnaelTaddese@users.noreply.github.com>
2024-08-21 06:28:41 -0400

    Time: 45 ms (92.8%), Space: 11.4 MB (100%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -6,6 +6,10 @@ class Solution(object):
         :rtype: int
         """
         total_sum = sum(nums)
+        # making sure if we actually can acheive the target
+        # for example for the test case [1] : 2
+        if (total_sum + target) % 2 != 0 or total_sum < abs(target):
+            return 0
 
         target_sum = (total_sum + target) // 2
 
