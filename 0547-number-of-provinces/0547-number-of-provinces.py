9a51e3836e549bfbce4b83cb6532cf9033b8e774
Natnael Taddese Teferra <101579499+NatnaelTaddese@users.noreply.github.com>
2024-09-03 11:17:21 -0400

    Time: 171 ms (63.73%), Space: 12.1 MB (86.27%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -5,24 +5,32 @@ class Solution(object):
         :rtype: int
         """
         n = len(isConnected)
-        visited = set()
-        provinces = 0
-
-        def dfs(city):
-            stack = [city]
-
-            while stack:
-                current = stack.pop()
-                for neighbor in range(n):
-                    if isConnected[current][neighbor] == 1 and neighbor not in visited:
-                        visited.add(neighbor)
-                        stack.append(neighbor)
-
-        for city in range(n):
-            if city not in visited:
-                dfs(city)
-                provinces += 1
+        parent = [i for i in range(n)]
+        rank = [1] * n
+        
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+        
+        def union(x, y):
+            rootX = find(x)
+            rootY = find(y)
+            
+            if rootX != rootY:
+                if rank[rootX] > rank[rootY]:
+                    parent[rootY] = rootX
+                elif rank[rootX] < rank[rootY]:
+                    parent[rootX] = rootY
+                else:
+                    parent[rootY] = rootX
+                    rank[rootX] += 1
         
 
-        return provinces
-            
+        for i in range(n):
+            for j in range(i + 1, n):
+                if isConnected[i][j] == 1:
+                    union(i, j)
+        
+        provinces = set([find(i) for i in range(n)])
+        return len(provinces)
