13e346dada74aa4b9417f0f710b43372c6390dd4
Natnael Taddese Teferra <101579499+NatnaelTaddese@users.noreply.github.com>
2024-11-02 00:45:21 +0800

    Time: 156 ms (70.66%), Space: 18 MB (37.01%) - LeetHub

---
+++ 0941-valid-mountain-array/0941-valid-mountain-array.py
@@ -0,0 +1,23 @@
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        peak_found = False
+        peak = 0
+
+        if len(arr) < 3:
+            return False
+
+        for i in range(len(arr) - 1):
+            if not peak_found and arr[i + 1] > arr[i]:
+                continue
+            if peak_found:
+                if arr[i+1] < arr[i]:
+                    continue
+                else:
+                    return False
+            
+            if i > 0 and arr[i + 1] < arr[i]:
+                peak_found = True
+            else:
+                return False
+        
+        return peak_found
